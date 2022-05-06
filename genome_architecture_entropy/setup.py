e3f350cd91809e78c1fd812ecc8cf82d0afb63c6
rnschmdlr <rnschmdlr@github.com>
2022-05-06 17:24:42 +0200

    renamed

--- genome_architecture_entropy/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import Extension, setup
-from Cython.Build import cythonize
-import numpy
-
-# define an extension that will be cythonized and compiled
-ext = Extension(name="cosegregation_internal", sources=["cosegregation_internal.pyx"])
-setup(ext_modules=cythonize(ext), include_dirs=[numpy.get_include()])
