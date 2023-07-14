b8d52e345c9d1ca00324255d10b1ae7226ffb6d7
rnschmdlr <rnschmdlr@github.com>
2023-07-14 11:09:23 +0200

    added more methods

--- genome_architecture_entropy/src/entropy/shannon_entropies/sliding_window.py
+++ genome_architecture_entropy/src/entropy/shannon_entropies/sliding_window.py
@@ -1,8 +1,12 @@
 # %%
+import os
 import sys
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
+
+os.chdir('/Users/pita/Documents/Rene/GAM_project/genome_architecture_entropy/src/entropy/shannon_entropies/')
+
 
 import compute_2d_entropies as em
 import plotting as plot
@@ -17,14 +21,22 @@ def load_data(filename):
     return seg_mat
 
 
-def region_entropy_plot(y, x, size, methods):
+def scale(a):
+    a = a - np.nanmin(a)
+    a = a - np.diag(np.diag(a))
+    vmax = np.nanmax(a)
+    # divide calculation anywhere 'where' a does not equal zero
+    a = np.divide(a, vmax, out=np.zeros_like(a), where = vmax != 0)
+
+    return a
+
+
+def region_entropy_plot(seg_mat, y, x, size, methods, offset=60, resolution=0.05):
     #print('x:', y, 'to', y+size, '\ny:', x, 'to', x+size)
     if x < 0 or y < 0:
         print('no data below 3 Mb')
         sys.exit()
 
-    offset = 60
-    resolution = 0.05
     vmin = 0
     vmax = 1
 
@@ -52,23 +64,15 @@ def region_entropy_plot(y, x, size, methods):
         #The pointwise mutual information represents a quantified measure for how much more- or less likely we are to see the two events co-occur, 
         # given their individual probabilities, and relative to the case where the two are completely independent.
         npmi_mat = em.npmi_2d_fast(seg_mat_region.astype(int), seg_mat_region.astype(int))
+        print(f'NPMI at contact loci: {np.sum(npmi_mat[15:17, 20:22]):.4f}')
+        npmi_mat[npmi_mat < 0] = np.nan #positive NPMI
         plot.slow_raster(npmi_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Normalised Pointwise Mutual Information')
-
-    if 'mi' in methods:
-        je_mat = em.all_joint_entropy(seg_mat_region)
-        mi_mat = em.mutual_information(je_mat)
-        plot.slow_raster(mi_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Mutual Information')
-
-        if 'rajski' in methods:
-            # distance in information of the two partitions
-            rajski_mat = 1 - mi_mat / je_mat
-            plot.slow_raster(rajski_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Rajski distance (Nnrm. variation of information)')
 
     if 'ncmi' in methods:
         ncmi_mat = em.normalized_mutual_information(seg_mat_region)
         plot.slow_raster(ncmi_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Normalised Corrected Mutual Information')
-
-    if 'vs' in methods:
+    
+    if 'npmi vs ncmi' in methods or 'ncmi vs npmi' in methods:
         npmi_mat = em.npmi_2d_fast(seg_mat_region.astype(int), seg_mat_region.astype(int))
         ncmi_mat = em.normalized_mutual_information(seg_mat_region)
 
@@ -85,15 +89,55 @@ def region_entropy_plot(y, x, size, methods):
         
         plot.slow_raster(ratio[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='npmi vs ncmi')
 
+    if 'mi' in methods or 'iqr' in methods or 'rajski' or 'iqr vs npmi' in methods or 'npmi vs iqr' in methods:
+        # measures dependence: amount of information obtained about one random variable by observing the other random variable
+        je_mat = em.all_joint_entropy(seg_mat_region)
+        mi_mat = em.mutual_information(je_mat)
+
     if 'je' in methods:
-        # amount by which the uncertainty of one random variable is reduced due to the knowledge of another
+        # measures total uncertainty: amount by which the uncertainty of one random variable is reduced due to the knowledge of another
         je_mat = em.all_joint_entropy(seg_mat_region)
         mean_je_mat = np.mean(je_mat[x:x+size, y:y+size])
-        #je_mat = scale(je_mat)
-        #np.fill_diagonal(je_mat, 0)
         plot.slow_raster(je_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Joint Entropy')
         print('Average joint entropy:', np.round(mean_je_mat, 4))
+        
+    if 'mi' in methods:
+        mi_mat_ = mi_mat
+        #np.fill_diagonal(mi_mat_, 0)
+        #mi_mat_ = scale(mi_mat_)
+        #np.fill_diagonal(mi_mat_, 1)
+        plot.slow_raster(mi_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Mutual Information')
 
+    if 'rajski' in methods:
+        # metric distance in information of the two partitions
+        # index of similarity for two nominal variables with discrete distributions
+        rajski_mat = 1 - mi_mat / je_mat
+        plot.slow_raster(rajski_mat[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Rajski distance (Norm. variation of information)')
+
+    if 'iqr' in methods:
+        # information quality ratio
+        # probability that signal will be perfectly reconstructed without losing of information
+        # probability of conveying a piece of information from X to Y
+        iqr_mat = mi_mat / je_mat
+        print(f'IQR at contact loci: {np.sum(iqr_mat[15:17, 20:22]):.4f}')
+        #np.fill_diagonal(iqr_mat, 0)
+        #iqr_mat = scale(iqr_mat)
+        #np.fill_diagonal(iqr_mat, 1)
+        plot.slow_raster(iqr_mat[x:x+size, y:y+size], vmin, np.max(iqr_mat), xticklabels, yticklabels, title='Information Quality Ratio')
+
+    if 'iqr vs npmi' in methods or 'npmi vs iqr' in methods:
+        npmi_mat = em.npmi_2d_fast(seg_mat_region.astype(int), seg_mat_region.astype(int))
+        iqr_mat = mi_mat / je_mat
+        #np.fill_diagonal(iqr_mat, 0)
+        #iqr_mat = scale(iqr_mat)
+        #np.fill_diagonal(npmi_mat, 0)
+        #npmi_mat = scale(npmi_mat)
+        ratio = np.abs(npmi_mat * iqr_mat)
+        print(f'IQR * NPMI at contact loci: {np.sum(ratio[15:17, 20:22]):.4f}')
+        #np.fill_diagonal(ratio, 0)
+        #ratio = scale(ratio)
+        #np.fill_diagonal(ratio, 1)
+        plot.slow_raster(ratio[x:x+size, y:y+size], vmin, vmax, xticklabels, yticklabels, title='Highest scoring in NPMI and IQR')
 
 
 def sliding_window_measures(segreg_data, start, end, step, size_range, methods):
@@ -205,20 +249,39 @@ def sliding_window_measures(segreg_data, start, end, step, size_range, methods):
     return df
 
 
+
 # %%
-'''Load data'''
+'''2D Cosegregation analysis: experimental data'''
 seg_mat = load_data('/Users/pita/Documents/Rene/GAM_project/genome_architecture_entropy/data/experimental/Curated_GAM_mESCs_46C_real_1NP_at50000.passed_qc_fc5_cw6_s11.table')
-
-
-# %% 
-'''2D Cosegregation measure figure'''
+## region coordinates
 xmb, ymb = 1302, 1302
 size = 6
-methods = ['ncmi', 'mi', 'rajski']
+# conversion to matrix coordinates
+offset = 60
+resolution = 0.05
 x = int(xmb * 100 / 5 - 60)
 y = int(ymb * 100 / 5 - 60)
 s = int(size * 100 / 5)
-matrix = region_entropy_plot(x, y, s, methods)
+
+methods = ['npmi', 'je', 'iqr', 'mi']
+matrix = region_entropy_plot(seg_mat, x, y, s, methods, offset, resolution)
+
+
+
+# %%
+'''2D Cosegregation analysis: toy model data'''''
+# load data
+path_model = '/Users/pita/Documents/Rene/GAM_project/genome_architecture_entropy/data/toymodels/model8/'
+models = ['modelA', 'modelB', 'modelC', 'modelD']
+model_seg = '_seg_mats_1000_t1'
+for model in models:
+    seg_mat = np.load(path_model + model + model_seg + '.npy')
+    seg_mat_ = np.reshape(seg_mat, newshape=(100 * 10, 36), order='F')
+    print(model)
+
+    methods = ['iqr', 'npmi']
+    matrix = region_entropy_plot(seg_mat_.T, 0, 0, 36, methods, offset=0, resolution=1)
+
 
 
 # %%
